f8b602eb8a3d910effac554dd116b29164a0b491
Shumax <maxcastro.mv@gmail.com>
2020-04-07 19:34:22 -0300

    finalizando front

--- web/src/App.js
+++ web/src/App.js
@@ -1,35 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import api  from './services/api';
 import './App.css';
 import './global.css';
 import './Sidebar.css';
 import './Main.css';
 
 function App() {
+  const [devs, setDevs] = useState([]);
+  const [github_username, setGitHub_Username] = useState('');
+  const [techs, setTechs] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const {latitude, longitude} = position.coords;
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+         console.log(err);
+      },
+      {
+        timeout: 30000,
+      }
+    )
+  }, []);
+
+  useEffect(() => {
+    async function carregaDevs(){
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
+    carregaDevs();
+  });
+
+  async function handleAddDev(e){
+    e.preventDefault();
+
+    const response = await api.post('/devs',{
+      github_username,
+      techs,
+      latitude,
+      longitude,
+    })
+    setGitHub_Username('');
+    setTechs('');
+    setDevs([...devs, response.data]);
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div class="input-block">
+        <form onSubmit={handleAddDev}>
+          <div className="input-block">
             <label htmlFor="github_username">Usuário do GitHub</label>
-            <input name="github_username" id="github_username" required />
+            <input 
+              name="github_username" 
+              id="github_username" 
+              required 
+              value={github_username}
+              onChange={e=> setGitHub_Username(e.target.value)}
+            />
           </div>
 
-          <div class="input-block">
+          <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
+            <input 
+              name="techs" 
+              id="techs" 
+              required 
+              value={techs}
+              onChange={e=> setTechs(e.target.value)}  
+            />
           </div>
 
           <div className="input-group">
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
+              <input 
+                type="number" 
+                name="latitude" 
+                id="latitude" 
+                required 
+                value={latitude}
+                onChange={e=> setLatitude(e.target.value)}
+              />
             </div>
 
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
+              <input 
+                type="number" 
+                name="longitude" 
+                id="longitude" 
+                required 
+                value={longitude}
+                onChange={e=> setLongitude(e.target.value)}
+              />
             </div>
           </div>
 
@@ -38,50 +108,25 @@ function App() {
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-                <img src="https://avatars0.githubusercontent.com/u/56134989?s=460&v=4" alt="Max Castro"/>
-                <div className="user-info">
-                  <strong>Max Castro</strong>
-                  <span>PHP, MySql</span>
-                </div>
-                <p>Desenvolvedor Web apaixonado por programação</p>
-                <a href="https://github.com/Shumax">Acessar perfil no GitHub</a>
-            </header>
-          </li>
-          <li className="dev-item">
-            <header>
-                <img src="https://avatars0.githubusercontent.com/u/56134989?s=460&v=4" alt="Max Castro"/>
-                <div className="user-info">
-                  <strong>Max Castro</strong>
-                  <span>PHP, MySql</span>
-                </div>
-                <p>Desenvolvedor Web apaixonado por programação</p>
-                <a href="https://github.com/Shumax">Acessar perfil no GitHub</a>
-            </header>
-          </li>
-          <li className="dev-item">
-            <header>
-                <img src="https://avatars0.githubusercontent.com/u/56134989?s=460&v=4" alt="Max Castro"/>
-                <div className="user-info">
-                  <strong>Max Castro</strong>
-                  <span>PHP, MySql</span>
-                </div>
-                <p>Desenvolvedor Web apaixonado por programação</p>
-                <a href="https://github.com/Shumax">Acessar perfil no GitHub</a>
-            </header>
-          </li>
-          <li className="dev-item">
-            <header>
-                <img src="https://avatars0.githubusercontent.com/u/56134989?s=460&v=4" alt="Max Castro"/>
-                <div className="user-info">
-                  <strong>Max Castro</strong>
-                  <span>PHP, MySql</span>
-                </div>
-                <p>Desenvolvedor Web apaixonado por programação</p>
-                <a href="https://github.com/Shumax">Acessar perfil no GitHub</a>
-            </header>
-          </li>
+          {devs.map(
+            dev => (
+              
+                <li key={dev._id} className="dev-item">
+                  <header>
+                      <img src={dev.avatar_url} alt={dev.nome}/>
+                      <div className="user-info">
+                        <strong>{dev.nome}</strong>
+                        <span>{dev.techs.join(', ')}</span>
+                      </div>
+                      <p>{dev.bio}</p>
+                      <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no GitHub</a>
+                  </header>
+                </li>
+              
+            )
+          )}
+         
+        
          </ul>
 
 
